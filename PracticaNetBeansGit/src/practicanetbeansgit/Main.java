9803474e95fc39077dec34110d44870c3e9c08a7
Marc <miemail@gmail.com>
2025-12-01 08:16:24 +0100

    He añadido la clase Main con un saludo personalizado.

---
+++ PracticaNetBeansGit/src/practicanetbeansgit/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package practicanetbeansgit;
+
+/**
+ *
+ * @author 6DAWM
+ * @version 1.0
+ * @see <a href=""></a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola desde NetBeans y Git! Soy Marc Guillem Ballester.");
+    }
+}
